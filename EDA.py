ec0fa2b671e236b28e06ab71eff3a508b1d3f8c1
ArgumedoHector <109697217+ArgumedoHector@users.noreply.github.com>
2023-03-02 18:36:58 -0300

    Add files via upload

---
+++ EDA.py
@@ -0,0 +1,32 @@
+# %%
+import streamlit as st
+import plotly.express as px
+import pandas_profiling
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+import os 
+
+if os.path.exists('./dataset.csv'): 
+    df = pd.read_csv('dataset.csv', index_col=None)
+
+with st.sidebar: 
+    st.image("https://abdatum.com/media/images/analisis-exploratorio-datos-cover.png")
+    st.title("EDA-APP")
+    choice = st.radio("Panel",["Subir","EDA"])
+    st.info("Esta app te ayuda realizando un Analisis Exploratorio de Datsos")
+
+if choice == "Subir":
+    st.title("Sube tu Dataset")
+    file = st.file_uploader("")
+    if file: 
+        df = pd.read_csv(file, index_col=None)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
+
+if choice == "EDA": 
+    st.title("Analisis Exploratorio de Datos")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
+
+
+
